2becdf2b9ac94530be66b0039954a8407b17bd4c
roclmbr <steve.adamson@nbcc.ca>
2016-06-23 20:34:55 -0300

    saved master branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622122318) do
+ActiveRecord::Schema.define(version: 20160623233033) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160622122318) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
