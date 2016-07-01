05baab0c3f5456d8946f2d431ee623960e05651f
roclmbr <steve.adamson@nbcc.ca>
2016-07-01 19:25:59 -0300

    Finished requirements for finance tracker

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
     
     devise_for :users, :controllers => { :registrations => "user/registrations" } 
   resources :user_stocks, except: [:show, :edit, :update]
-  root 'welcome#index'
+  resources :users, only: [:show] 
+  resources :friendships  
   resources :articles
   resources :images
   resources :categories, except: [:destroy]
+    
+  root 'welcome#index'
   
   get 'pages/about', to: 'pages#about'
   get 'pages/climbing', to: 'pages#climbing'
@@ -14,5 +17,8 @@ Rails.application.routes.draw do
   get 'pages/users', to: 'pages#users'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
-   
+  get 'my_friends', to: "users#my_friends" 
+  get 'search_friends', to: "users#search"
+    
+  post 'add_friend', to: "users#add_friend"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630181105) do
+ActiveRecord::Schema.define(version: 20160630190104) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20160630181105) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "images", force: :cascade do |t|
