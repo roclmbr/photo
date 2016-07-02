75ffcca06262c4a85ad0850e527952c6b5ff32da
roclmbr <steve.adamson@nbcc.ca>
2016-07-02 10:09:48 -0300

    testing email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     
-    devise_for :users, :controllers => { :registrations => "user/registrations" } 
+  devise_for :users, :controllers => { :registrations => "user/registrations" } 
+    
   resources :user_stocks, except: [:show, :edit, :update]
   resources :users, only: [:show] 
   resources :friendships  
