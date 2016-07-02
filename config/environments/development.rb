cd6d949d39266684cf7a9395d9f60cdb0355f866
steve <mail@steveadamson.com>
2016-07-02 16:13:11 -0300

    Fixed ruby file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+    config.action_mailer.default_url_options = { :host => 'mteverest.myip.org', :protocol => 'http'}
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
